226435b0e5663416d20d8de79943644a941e77f2
Kendrim87 <226069897+Kendrim87@users.noreply.github.com>
2026-01-14 10:20:15 +0100

    Actualizar permisos de archivos y eliminar comentarios innecesarios en la configuración de Vue

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  // Configurar publicPath para usar rutas relativas
-  // Permite acceder desde subcarpetas como /DWEC/UT06/Tarea6_2_1/vue3/dist/
   publicPath: './'
 })
